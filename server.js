8362c58539959a38fd47bb6f198d72ed7ddba0f6
Chris Sevilleja <chris@scotch.io>
2017-01-28 21:39:53 -0800

    adding first files

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express'),
+  app         = express(),
+  path        = require('path'),
+  port        = process.env.port || 8080;
+
+// settings
+app.use(express.static(__dirname + '/dist'));
+
+// routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+// start the server
+app.listen(port, () => console.log('Running!'));
