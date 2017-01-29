f0762e9c0b225c877601b6d165eee50163a894c2
Chris Sevilleja <chris@scotch.io>
2017-01-28 23:55:03 -0800

    more prod ready

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const express = require('express'),
   app         = express(),
   path        = require('path'),
+  compression = require('compression'),
   port        = process.env.PORT || 8080;
-
-function forceSSL() {
-  return (req, res, next) => {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      const url = `https://${req.get('Host')}${req.url}`;
-      return res.redirect(url);
-    }
-
-    next();
-  }
-}
 
 // settings
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use(forceSSL);
+app.use(compression());
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] == 'http') {
+    const host = req.get('Host');
+    const url  = `https://${host}${req.url}`;
+    return res.redirect(url);
+  }
+
+  next();
+});
 
 // routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'dist', 'index.html')));
