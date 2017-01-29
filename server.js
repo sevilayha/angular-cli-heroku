218180efc2dac2193bef046c749810867bf365bc
Chris Sevilleja <chris@scotch.io>
2017-01-28 23:19:18 -0800

    testing

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const express = require('express'),
   port        = process.env.PORT || 8080;
 
 // settings
-app.use(express.static(__dirname + '/dist'));
-
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 // start the server
 app.listen(port, () => console.log('Running!'));
